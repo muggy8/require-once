9c78f257dce6f0d7984e487b986ba6b4fc0683fa
muggy8 <muggy8@gmail.com>
2017-04-19 23:58:53 -0700

    started on project

---
+++ require-once.js
@@ -0,0 +1,8 @@
+(function(context){
+	var alreadyLoaded = [];
+	context.requireOnce = context.require_once = function(dependencies, callback){
+		for (var i = 0; i < dependencies.length; i++) {
+			
+		}
+	}
+})(this);
