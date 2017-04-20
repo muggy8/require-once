17072b10eb9a0a7b147162ca6226e6b6c0c2c565
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-04-20 10:25:54 -0700

    made a safer eval function
    safer eval executes on the highest standalone scope away from the other
    scopes and will attempt to not inherit anything to make injection
    attacks via eval() harder

--- require-once.js
+++ require-once.js
@@ -1,8 +1,26 @@
 (function(context){
-	var alreadyLoaded = [];
-	context.requireOnce = context.require_once = function(dependencies, callback){
-		for (var i = 0; i < dependencies.length; i++) {
-			
-		}
-	}
+    function saferEval(code){
+        if (this !== context){
+            return safeEval.apply(context, code);
+        }
+        var module = {}
+        var exporter = module.exports = module.export = function(output){
+            module.exports = output;
+        }
+        eval(code);
+        if (module.exports != exporter){
+            return module.exports;
+        }
+
+    }
+
+    var scopeProtector = function(){
+        var alreadyLoaded = [];
+    	context.requireOnce = context.require_once = function(dependencies, callback){
+    		for (var i = 0; i < dependencies.length; i++) {
+
+    		}
+    	}
+    };
+    scopeProtector();
 })(this);
