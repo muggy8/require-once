11224bf679427f3602e5e13805e693189ef52190
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-04-20 11:56:07 -0700

    added ajax loader and working on require logic

--- require-once.js
+++ require-once.js
@@ -3,6 +3,7 @@
         if (this !== context){
             return safeEval.apply(context, code);
         }
+
         var module = {}
         var exporter = module.exports = module.export = function(output){
             module.exports = output;
@@ -11,16 +12,53 @@
         if (module.exports != exporter){
             return module.exports;
         }
-
     }
 
     var scopeProtector = function(){
-        var alreadyLoaded = [];
+        var attemptAjaxGet = function(url, callback, attempts){
+            attempts = attempts || 1;
+
+            var xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+                    if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
+                        callback(xmlhttp.responseText);
+                    }
+                    else if (attempts < 5) {
+                        setTimeOut(function(){
+                            attemptAjaxGet(url, callback, attempts+1);
+                        }, attempts*100);
+                    }
+                    else {
+                        callback(null);
+                    }
+                }
+            };
+
+            xmlhttp.open("GET", dependencies[i], true);
+            xmlhttp.send();
+        }
+
+        var alreadyLoaded = {};
     	context.requireOnce = context.require_once = function(dependencies, callback){
     		for (var i = 0; i < dependencies.length; i++) {
-
+                var resource = dependencies[i];
+                if (alreadyLoaded[resource]) {
+                    callback(alreadyLoaded[resource].exported)
+                }
+                else {
+                    attemptAjaxGet(resource, function(data){
+                        if (data){
+                            alreadyLoaded[resource] = {
+                                lable: resource,
+                                exported: saferEval(data)
+                            };
+                        }
+                    });
+                }
     		}
-    	}
+    	};
     };
     scopeProtector();
 })(this);
