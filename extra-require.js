e70589c58f6ed84f7fd9ce831fde9bd0545cb6be
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-04-21 16:51:17 -0700

    trying to use require once outside in a required page

---
+++ extra-require.js
@@ -0,0 +1,17 @@
+requireOnce([
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+    'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css',
+    'https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+    'https://ajax.googleapis.com/ajax/libs/hammerjs/2.0.8/hammer.min.js',
+
+], function(jquery, uiCSS, jqueryUI, hammerjs){
+    $("head")
+        .append(
+            $("<style></style>").text(uiCSS)
+        )
+
+    new Hammer($("body")[0])
+        .on("tap", function(ev){
+            console.log("Eeeww I've been touched");
+        });
+});
