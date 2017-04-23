acf123a4b697ff89699a5643c2e8adbd580f13a4
muggy8 <muggy8@gmail.com>
2017-04-22 19:57:12 -0700

    manual minify

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 if (typeof requireOnce == 'undefined') var requireOnce = require("./require_once.js");
 
 requireOnce([
-	{server: "keygen", browser: "https://unpkg.com/keygen@latest"}
-], function(keygen){
-	console.log(keygen.url(64));
+	{server: "do-async", browser: "https://unpkg.com/do-async@latest"}, 
+	{server: "method-overload", browser: "https://unpkg.com/method-overload@latest"}, 
+	"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", 
+	
+], function(doAsync, overload, bootstrapCss){
+	doAsync(function(){
+		var chain = this;
+		setTimeout(this.pass, 1000);
+	}).then(function(){
+		console.log(overload);
+		console.log(bootstrapCss);
+	})
 })
