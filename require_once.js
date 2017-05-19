a15b871e5d664abebbbb8d51ada86ceb12ad89d6
Billy (muggy8) Li <billy@sitemaxsystems.combilly@sitemaxsystems.combilly@sitemaxsystems.com>
2017-05-19 11:43:26 -0700

    moved  code around alot ._.

--- require_once.js
+++ require_once.js
@@ -1,199 +1,186 @@
 (function(context, safeEval){
-    function saferEval(code){
-        if (this != context){
-            return saferEval.call(context, code)
-        }
+    var registry = {}
 
-        var module = {},
-            exporter = module.exports = module.export = function(output){
-                module.exports = output
-            },
-            results = eval(code)
+	var seekOrGet = function(url, callback){
+        var requestUrl = url
+		var registryEntry = registry[url]
 
-        // is node package and get the module.exports and return it to the caller
-        if (module.exports != exporter){
-            return module.exports
-        }
-        // maybe the code returned something? return that to caller
-		else if (results) {
-			return results
+		// already loaded and is in cache
+		if (registryEntry && typeof registryEntry.result !== 'undefined'){
+			callback(registryEntry.request)
 		}
-        // code has no outputs, let the caller know that the code evaluated successfully.
-        return true
-    }
 
-    (function(){
-        var registry = {}
+		// is currently in the process of fetching
+		else if (registryEntry && registryEntry.result === 'undefined'){
+			registryEntry.waiters.push(callback)
+		}
 
-		var seekOrGet = function(url, callback){
-            var requestUrl = url
-			var registryEntry = registry[url]
-
-			// already loaded and is in cache
-			if (registryEntry && typeof registryEntry.result !== 'undefined'){
-				callback(registryEntry.request)
+		// is a completely new URL not known yet
+		else {
+			var queue = registry[url] = {
+				waiters: [callback],
+				attempts: 1
 			}
 
-			// is currently in the process of fetching
-			else if (registryEntry && registryEntry.result === 'undefined'){
-				registryEntry.waiters.push(callback)
-			}
+			function attemptConnection (){
+				var connection = queue.request = new XMLHttpRequest()
 
-			// is a completely new URL not known yet
-			else {
-				var queue = registry[url] = {
-					waiters: [callback],
-					attempts: 1
-				}
+				connection.onreadystatechange = function(){
 
-				function attemptConnection (){
-					var connection = queue.request = new XMLHttpRequest()
+					if (connection.readyState == XMLHttpRequest.DONE){
 
-					connection.onreadystatechange = function(){
-
-						if (connection.readyState == XMLHttpRequest.DONE){
-
-							if (connection.status >= 200 && connection.status < 300) {
-								// success
-								queue.result = 'success'
-								for (var i = 0; i < queue.waiters.length; i++){
-									queue.waiters[i](connection)
-								}
-							}
-
-                            else if (connection.status >= 300 && connection.status < 400) {
-
-                                requestUrl = connection.getResponseHeader("Location") || url
-                                if (requestUrl != url){
-                                    attemptConnection()
-                                }
-                                else {
-                                    queue.attempts++
-    								setTimeout(attemptConnection, queue.attempts*100)
-                                }
-                            }
-
-							else if (queue.attempts < 5){
-								// retry this process with timeout
-								queue.attempts++
-								setTimeout(attemptConnection, queue.attempts*100)
-							}
-
-							else {
-								// asset failed to load.
-								for (var i = 0; i < queue.waiters.length; i++){
-									queue.waiters[i](connection)
-								}
-                                queue.result = 'failed'
+						if (connection.status >= 200 && connection.status < 300) {
+							// success
+							queue.result = 'success'
+							for (var i = 0; i < queue.waiters.length; i++){
+								queue.waiters[i](connection)
 							}
 						}
-					}
-					connection.open("GET", requestUrl, true)
-					connection.send()
-				}
-				attemptConnection()
-			}
-		}
 
-        if (document && XMLHttpRequest){ // dont do this if in node
-            var waitingForDoc = []
-            document.addEventListener("readystatechange", function(){
-                if (document.readyState == "complete"){
-                    for(var i = 0; i < waitingForDoc.length; i++){
-                        waitingForDoc[i]()
-                    }
+                        else if (connection.status >= 300 && connection.status < 400) {
+
+                            requestUrl = connection.getResponseHeader("Location") || url
+                            if (requestUrl != url){
+                                attemptConnection()
+                            }
+                            else {
+                                queue.attempts++
+								setTimeout(attemptConnection, queue.attempts*100)
+                            }
+                        }
+
+						else if (queue.attempts < 5){
+							// retry this process with timeout
+							queue.attempts++
+							setTimeout(attemptConnection, queue.attempts*100)
+						}
+
+						else {
+							// asset failed to load.
+							for (var i = 0; i < queue.waiters.length; i++){
+								queue.waiters[i](connection)
+							}
+                            queue.result = 'failed'
+						}
+					}
+				}
+				connection.open("GET", requestUrl, true)
+				connection.send()
+			}
+			attemptConnection()
+		}
+	}
+
+    if (document && XMLHttpRequest){ // dont do this if in node
+        var waitingForDoc = []
+        document.addEventListener("readystatechange", function(){
+            if (document.readyState == "complete"){
+                for(var i = 0; i < waitingForDoc.length; i++){
+                    waitingForDoc[i]()
+                }
+            }
+        })
+        function waitForDocReady (callback) {
+            if (document.readyState == "complete"){
+                callback()
+            }
+            else {
+                waitingForDoc.push(callback)
+            }
+        }
+
+        var xhrs = []
+        var xhrReadyCallbacks = []
+        XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send
+        XMLHttpRequest.prototype.send = function(){
+            var xhr = this
+            var requestIndex = xhrs.push(xhr) - 1
+            xhr.addEventListener("loadend", function(){
+                // if something is waiting for xhr requests to finish call the first thing so it can start it's resolution cycle
+                if (xhrReadyCallbacks.length){
+                    xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
                 }
             })
-            function waitForDocReady (callback) {
-                if (document.readyState == "complete"){
-                    callback()
+            xhr._send()
+        }
+    }
+
+	context.requireOnce = context.require_once = function(dependencies, callback, failed){
+		if (!callback){
+			throw "Success callback not defined"
+		}
+		failed = failed || function(){}
+		var obtainedDependencies = [],
+            numberReturned = 0,
+            afterEvaluatedXhrsAreDone = function(){
+                xhrReadyCallbacks.splice(xhrReadyCallbacks.length - 1, 1)
+
+                var noXhrsLoading = xhrs.reduce(function(truthness, xhr){
+                                       //xhr item is an XMLHttpRequest object  || xhr item is a script tag
+                    return truthness && (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == "complete")
+                }, true)
+
+                if (noXhrsLoading){
+                    // resolve the the call
+                    // call the next item in the chain
+                    console.log("execution got here so that's good", obtainedDependencies);
                 }
                 else {
-                    waitingForDoc.push(callback)
+                    xhrReadyCallbacks.push(afterEvaluatedXhrsAreDone)
+                }
+            },
+            ifAllDependenciesLoaded = function(){
+                if (numberReturned == dependencies.length){ // all dependencies have returned
+                    obtainedDependencies.forEach(function(wrapper){
+                        if (wrapper && wrapper.xhr.getResponseHeader("Content-Type").match(/javascript/i)){
+                            wrapper.evaluater = safeEval(wrapper.xhr.responseText)
+                            xhrReadyCallbacks.push(afterEvaluatedXhrsAreDone)
+
+                            var noXhrsLoading = xhrs.reduce(function(truthness, xhr){
+                                return truthness && (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == "complete")
+                                                   //xhr item is an XMLHttpRequest object  || xhr item is a script tag
+                            }, true)
+
+                            if (noXhrsLoading){
+                               xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
+                            }
+                        }
+                    })
                 }
             }
 
-            var xhrs = []
-            var xhrReadyCallbacks = []
-            XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send
-            XMLHttpRequest.prototype.send = function(){
-                var xhr = this
-                var requestIndex = xhrs.push(xhr) - 1
-                xhr.addEventListener("loadend", function(){
-                    // if something is waiting for xhr requests to finish call the first thing so it can start it's resolution cycle
-                    if (xhrReadyCallbacks.length){
-                        xhrReadyCallbacks[xhrReadyCallbacks.length - 1](xhr)
-                    }
-                })
-                xhr._send()
-            }
-        }
-
-
-    	context.requireOnce = context.require_once = function(dependencies, callback, failed){
-			if (!callback){
-				throw "Success callback not defined"
-			}
-			failed = failed || function(){}
-    		var obtainedDependencies = [],
-                numberReturned = 0
-
-			var attemptCallback = function(){
-				if (numberReturned == dependencies.length){ // all dependencies have returned
-					var callFail = false
-
-					obtainedDependencies.forEach(function(gotten){
-						if (!gotten && gotten === false){
-							callFail = true
-						}
-					})
-
-					if (callFail){
-						failed.apply(context, obtainedDependencies)
-					}
-					else {
-						callback.apply(context, obtainedDependencies)
-					}
+        dependencies.forEach(function(mixedDependency, index){
+			var dependency
+			if (typeof require != 'undefined' && typeof XMLHttpRequest == 'undefined'){ // inside node
+				dependency = mixedDependency.server
+				if (dependency){
+					obtainedDependencies[index] = require(dependency)
 				}
+				else {
+					obtainedDependencies[index] = false
+				}
+
+				numberReturned++
+                if (numberReturned == dependencies.length){ // all dependencies have returned
+    				callback.apply(context, obtainedDependencies)
+    			}
 			}
+			else if (XMLHttpRequest){ // inside browser
+				dependency = mixedDependency.browser || mixedDependency; // mixedDependency can be object or a URL string
 
-			//for (var i = 0; i < dependencies.length; i++) {
-            dependencies.forEach(function(mixedDependency, index){
-
-				var dependency
-				if (typeof require != 'undefined' && typeof XMLHttpRequest == 'undefined'){ // inside node
-					dependency = mixedDependency.server
-					if (dependency){
-						obtainedDependencies[index] = require(dependency)
+                seekOrGet(dependency, function(xhrObject){
+					if (xhrObject.status >= 200 && xhrObject.status < 300){
+						obtainedDependencies[index] = {xhr: xhrObject}
 					}
 					else {
 						obtainedDependencies[index] = false
 					}
 					numberReturned++
-					attemptCallback()
-				}
-				else if (XMLHttpRequest){ // inside browser
-					dependency = mixedDependency.browser || mixedDependency; // mixedDependency can be object or a URL string
-					seekOrGet(dependency, function(xhrObject){
-
-						numberReturned++
-
-						if (xhrObject.status >= 200 && xhrObject.status < 300){
-							obtainedDependencies[index] = xhrObject
-						}
-						else {
-							obtainedDependencies[index] = false
-						}
-
-						attemptCallback()
-					})
-
-				}
-
-            })
-
-    	}
-    })()
+                    ifAllDependenciesLoaded()
+				})
+			}
+        })
+	}
 
 	if (typeof module != 'undefined'){
 		module.exports = context.requireOnce
@@ -208,7 +195,7 @@
         var results = eval(code)
 
         return {
-            results: results,
+            output: results,
             module: module
         }
     }.bind(this)
