c2c4cdd2bc0f4159ceef646886068f751c0b0e81
muggy8 <muggy8@gmail.com>
2017-05-28 01:04:23 -0700

    udated script to not double execute scripts

--- require_once.js
+++ require_once.js
@@ -1,8 +1,8 @@
 (function(context, safeEval){
-    var registry = {}
+	var registry = {}
 
 	var seekOrGet = function(url, callback){
-        var requestUrl = url,
+		var requestUrl = url,
 			registryEntry = registry[url],
 			attemptConnection = function(queue){
 				var connection = queue.request = new XMLHttpRequest(),
@@ -16,7 +16,12 @@
 							// success
 							queue.result = 'success'
 							for (var i = 0; i < queue.waiters.length; i++){
-								queue.waiters[i](connection)
+								if (i == 0){
+									queue.waiters[i](queue)
+								}
+								else {
+									seekOrGet(url, queue.waiters[i])
+								}
 							}
 						}
 
@@ -38,11 +43,11 @@
 						}
 
 						else {
+							queue.result = 'failed'
 							// asset failed to load.
 							for (var i = 0; i < queue.waiters.length; i++){
-								queue.waiters[i](connection)
+								queue.waiters[i](queue)
 							}
-							queue.result = 'failed'
 						}
 					}
 				}
@@ -52,7 +57,7 @@
 
 		// already loaded and is in cache
 		if (registryEntry && typeof registryEntry.result !== 'undefined'){
-            callback(registryEntry.request, registryEntry.returnVal)
+			callback(registryEntry)
 		}
 
 		// is currently in the process of fetching
@@ -71,129 +76,127 @@
 		}
 	}
 
-    if (typeof document!= "undefined" && typeof XMLHttpRequest != "undefined"){ // dont do this if in node
-        var waitingForDoc = []
-        document.addEventListener("readystatechange", function(){
-            if (document.readyState == "complete"){
-                for(var i = 0; i < waitingForDoc.length; i++){
-                    waitingForDoc[i]()
-                }
-            }
-        })
-        function waitForDocReady (callback) {
-            if (document.readyState == "complete"){
-                callback()
-            }
-            else {
-                waitingForDoc.push(callback)
-            }
-        }
+	if (typeof document!= "undefined" && typeof XMLHttpRequest != "undefined"){ // dont do this if in node
+		var waitingForDoc = []
+		document.addEventListener("readystatechange", function(){
+			if (document.readyState == "complete"){
+				for(var i = 0; i < waitingForDoc.length; i++){
+					waitingForDoc[i]()
+				}
+			}
+		})
+		function waitForDocReady (callback) {
+			if (document.readyState == "complete"){
+				callback()
+			}
+			else {
+				waitingForDoc.push(callback)
+			}
+		}
 
-        var xhrs = []
-        var xhrReadyCallbacks = []
-        XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send
-        XMLHttpRequest.prototype.send = function(){
-            var xhr = this
-            var requestIndex = xhrs.push(xhr) - 1
-            xhr.addEventListener("loadend", function(){
-                // if something is waiting for xhr requests to finish call the first thing so it can start it's resolution cycle
-                if (xhrReadyCallbacks.length){
-                    xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
-                }
-            })
-            xhr._send()
-        }
-    }
+		var xhrs = []
+		var xhrReadyCallbacks = []
+		XMLHttpRequest.prototype._send = XMLHttpRequest.prototype.send
+		XMLHttpRequest.prototype.send = function(){
+			var xhr = this
+			var requestIndex = xhrs.push(xhr) - 1
+			xhr.addEventListener("loadend", function(){
+				// if something is waiting for xhr requests to finish call the first thing so it can start it's resolution cycle
+				if (xhrReadyCallbacks.length){
+					xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
+				}
+			})
+			xhr._send()
+		}
+	}
 
-	context.requireOnce = context.require_once = function(dependencies, callback, failed){
+	context.requireOnce = context.require_once = (context.requireOnce && context.require_once)? context.requireOnce : function(dependencies, callback, failed){
 		if (!callback){
 			throw new Error("Success callback not defined")
 		}
 		failed = failed || function(){}
 
 		var obtainedDependencies = [],
-            numberReturned = 0,
+			numberReturned = 0,
 			domWaiterSet = false,
 			domAlsoReady = function(){
+				// resolve "returnVal" for all dependencies
+				obtainedDependencies.forEach(function(opperator, index){
+					if (opperator && opperator.evaluater){
+						opperator.returnVal = opperator.returnVal || (opperator.evaluater.module.export != opperator.evaluater.module.exports)
+							? opperator.evaluater.module.exports
+							: (typeof opperator.evaluater.output == "undefined")? true : opperator.evaluater.output
+					}
+				})
+
+				// call success or fail appropriately
+				var successFlag = true;
+				var applyArray = obtainedDependencies.map(function(dependency){
+					if (!dependency){
+						return successFlag = false
+					}
+					else {
+						return dependency.returnVal
+					}
+				})
+
+				if (successFlag){
+					callback.apply(context, applyArray)
+				}
+				else {
+					failed.apply(context, applyArray)
+				}
+
 				//remove the last item in callback (self)
 				xhrReadyCallbacks.splice(xhrReadyCallbacks.length - 1, 1)
 
-                // resolve "returnVal" for all dependencies
-                obtainedDependencies.forEach(function(wrapper, index){
-                    var dependencyName = dependencies[index].browser || dependencies[index];
-
-                    registry[dependencyName].returnVal = wrapper.returnVal
-                    if (wrapper && wrapper.evaluater){
-                        registry[dependencyName].returnVal = wrapper.returnVal = (wrapper.evaluater.module.export != wrapper.evaluater.module.exports)
-                            ? wrapper.evaluater.module.exports
-                            : wrapper.evaluater.output
-                    }
-                })
-
-                // call success or fail appropriately
-                var successFlag = true;
-                var applyArray = obtainedDependencies.map(function(dependency){
-                    if (!dependency){
-                        return successFlag = false
-                    }
-                    else {
-                        return dependency.returnVal
-                    }
-                })
-
-                if (successFlag){
-                    callback.apply(context, applyArray)
-                }
-                else {
-                    failed.apply(context, applyArray)
-                }
-
-                // call the next item in the chain
-                if (xhrReadyCallbacks.length){
-                    xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
-                }
-            },
-            afterEvaluatedXhrsAreDone = function(){
+				// call the next item in the chain
+				if (xhrReadyCallbacks.length){
+					xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
+				}
+			},
+			afterEvaluatedXhrsAreDone = function(){
 				var noXhrsLoading = xhrs.reduce(function(truthness, xhr){
-                                       //xhr item is an XMLHttpRequest object  || xhr item is a script tag
-                    return truthness && (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == "complete")
-                }, true)
+									   //xhr item is an XMLHttpRequest object  || xhr item is a script tag
+					return truthness && (xhr.readyState == XMLHttpRequest.DONE || xhr.readyState == "complete")
+				}, true)
 
-                if (noXhrsLoading && !domWaiterSet){
+				if (noXhrsLoading && !domWaiterSet){
 					domWaiterSet = true
 					waitForDocReady(domAlsoReady)
 				}
 			},
-            ifAllDependenciesLoaded = function(){
-                if (numberReturned == dependencies.length){ // all dependencies have returned
-                    // eval all unevaluated javascripts
-                    obtainedDependencies.forEach(function(wrapper){
-                        if (
-                            wrapper && // xhr did not fail
-                            typeof wrapper.returnVal == "undefined" && // has not been evaluated already
-                            wrapper.xhr.getResponseHeader("Content-Type").match(/javascript/i) // is a javascript file
-                        ){
-                            wrapper.evaluater = safeEval(wrapper.xhr.responseText)
-                        }
-                        else if (
-                            wrapper && // xhr did not fail
-                            typeof wrapper.returnVal == "undefined" && // has not been evaluated already
-                            wrapper.xhr // file is not javascript
-                        ){
-                            wrapper.returnVal = wrapper.xhr.responseText
-                        }
-                    })
+			ifAllDependenciesLoaded = function(){
+				if (numberReturned == dependencies.length){ // all dependencies have returned
+					// eval all unevaluated javascripts
+					obtainedDependencies.forEach(function(opperator){
+						if (
+							opperator && // xhr did not fail
+							opperator.request.getResponseHeader("Content-Type").match(/javascript/i) && // is a javascript file
+							typeof opperator.evaluater == "undefined" // has not been evaluated already
 
-                    // add a callback to wait on any xhr requests that resulted from all the evals
-                    xhrReadyCallbacks.push(afterEvaluatedXhrsAreDone)
+						){
+							opperator.evaluater = safeEval(opperator.request.responseText)
+						}
+						else if (
+							opperator && // xhr did not fail
+							typeof opperator.returnVal != "undefined" && // has not been evaluated already
+							!opperator.request.getResponseHeader("Content-Type").match(/javascript/i)  // file is not javascript
+						){
+							opperator.returnVal = opperator.request.responseText
+						}
+					})
+
+					// add a callback to wait on any xhr requests that resulted from all the evals
+					xhrReadyCallbacks.push(afterEvaluatedXhrsAreDone)
 
 					// call the chain it's gonna check for additional XHRs and dom readiness anyways
 					xhrReadyCallbacks[xhrReadyCallbacks.length - 1]()
-                }
-            }
+				}
+			}
 
 		// actual logig for getting the dependencies and calling them
-        dependencies.forEach(function(mixedDependency, index){
+		dependencies.forEach(function(mixedDependency, index){
 			var dependency
 			if (typeof require != 'undefined' && typeof XMLHttpRequest == 'undefined'){ // inside node
 				dependency = mixedDependency.server
@@ -205,44 +208,44 @@
 				}
 
 				numberReturned++
-                if (numberReturned == dependencies.length){ // all dependencies have returned
-    				callback.apply(context, obtainedDependencies)
-    			}
+				if (numberReturned == dependencies.length){ // all dependencies have returned
+					callback.apply(context, obtainedDependencies)
+				}
 			}
 			else if (XMLHttpRequest){ // inside browser
 				dependency = mixedDependency.browser || mixedDependency; // mixedDependency can be object or a URL string
 
-                seekOrGet(dependency, function(xhrObject, cachedReturnVal){
-					if ((xhrObject.status >= 200 && xhrObject.status < 300) || xhrObject.status == 304){
-						obtainedDependencies[index] = {xhr: xhrObject, returnVal: cachedReturnVal}
+				seekOrGet(dependency, function(opperator){
+					if (opperator.result == "success"){
+						obtainedDependencies[index] = opperator //{xhr: xhrObject, returnVal: cachedReturnVal}
 					}
-					else {
+					else if (opperator.result = "failed"){
 						obtainedDependencies[index] = false
 					}
 					numberReturned++
-                    ifAllDependenciesLoaded()
+					ifAllDependenciesLoaded()
 				})
 			}
-        })
+		})
 	}
 
 	if (typeof module != 'undefined'){
 		module.exports = context.requireOnce
 	}
 })(
-    this,
-    function(code){
-        var module = {},
-            requireOnce = this.requireOnce,
-            require_once = this.require_once,
-            exporter = module.exports = module.export = function(output){
-                module.exports = output
-            },
-            results = eval(code)
+	this,
+	function(code){
+		var module = {},
+			requireOnce = this.requireOnce,
+			require_once = this.require_once,
+			exporter = module.exports = module.export = function(output){
+				module.exports = output
+			},
+			results = eval(code)
 
-        return {
-            output: results,
-            module: module
-        }
-    }
+		return {
+			output: results,
+			module: module
+		}
+	}
 )
